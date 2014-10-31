c79b9fc85a44d1ad458a2a0e3d0e2fa156224332
Pablovm1990 <pablovm1990@gmail.com>
2014-10-31 20:41:46 +0100

    changes

--- ejer8.py
+++ ejer8.py
@@ -1,15 +1,26 @@
 nomFich = raw_input("Fichero: ")
 #open files
 
+tabla={"ctt":"l","atg":"m","aca":"t","acg":"t","atc":"i","aac":"n",
+"ata":"i","agg":"r","cct":"p","act":"t","agc":"s","aag":"k","aga":"r",
+"cat":"h","aat":"n","att":"i","ctg":"l","cta":"l","ctc":"l","cac":"h",
+"aaa":"k","ccg":"p","agt":"s","cca":"p","caa":"q","ccc":"p","tat":"y",
+"ggt":"g","tgt":"c","cga":"r","cag":"q","tct":"s","gat":"d","cgg":"r",
+"ttt":"f","tgc":"c","ggg":"g","tag":"*","gga":"g","taa":"*","ggc":"g",
+"tac":"y","ttc":"f","tcg":"s","tta":"l","ttg":"l","tcc":"s","acc":"t",
+"tca":"s","gca":"a","gta":"v","gcc":"a","gtc":"v","gcg":"a","gtg":"v",
+"gag":"e","gtt":"v","gct":"a","tga":"*","gac":"d","cgt":"r","tgg":"w",
+"gaa":"e","cgc":"r"}
+
 cont=0
 f = open(nomFich)
 contA = contG = contC = contT = 0;
 baseCodonNum = 0
 baseCodon = ""
 codones = []
+aminoAcidos = []
 for line in f:
 	#processinng
-	
 	if(cont>4) :
 		for base in line :	
 			if(base == 'a') :
@@ -32,10 +43,12 @@ for line in f:
 			if baseCodonNum > 2 :
 				baseCodonNum = 0
 				codones.append(baseCodon)
+				aminoAcidos.append(tabla[baseCodon])
 				baseCodon = ""
 
 	cont+=1
 
+print aminoAcidos
 print codones
 #closing files
 f.close()
