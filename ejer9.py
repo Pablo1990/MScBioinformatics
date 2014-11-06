15fa946ea7e1b63440c29c410f36d3af222cf0a6
Pablovm1990 <pablovm1990@gmail.com>
2014-11-06 19:28:59 +0100

    exercise 12 done

--- ejer9.py
+++ ejer9.py
@@ -2,7 +2,6 @@ def factorial(number) :
 	total= 1
 	for i in range(1,number+1) :
 		total*=i
-
 	return total
 
 def factorialRecursive(number) :
@@ -10,6 +9,7 @@ def factorialRecursive(number) :
 		return 1
 	else :
 		return number * factorialRecursive(number-1)
+
 
 numFactorial = input("Number: ")
 print factorial(numFactorial)
