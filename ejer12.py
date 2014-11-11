d5c10f093aa4e36245e9706ebfb55456ea216048
Pablovm1990 <pablovm1990@gmail.com>
2014-11-11 18:32:05 +0100

    menu done

--- ejer12.py
+++ ejer12.py
@@ -21,30 +21,46 @@ def multipliMatrix(matrix1, matrix2) :
 #Predefined matrix
 #x = [[1,2],[4,5],[7,8],[10,11]]
 #y = [[1,2,5],[1,2,6]]
+salute = 1
 
-tam11 = input("Introduce number of files of matrix 1: ")
-tam12 = input("Introduce number of columns of matrix 1: ")
-tam21 = input("Introduce number of files of matrix 2: ")
-tam22 = input("Introduce number of columns of matrix 2: ")
+while salute!=3 :
+	print("Option 1: I want to enter two matrices with the standard input")
+	print("Option 2: I want to enter the name of two files wich are the matrices")
+	print("Option 3: I want you")
+	salute = input("Hi, what do you want to do? ")
+	
+	x=[]
+	y=[]
+	if(salute == 1) :
+		tam11 = input("Introduce number of files of matrix 1: ")
+		tam12 = input("Introduce number of columns of matrix 1: ")
+		tam21 = input("Introduce number of files of matrix 2: ")
+		tam22 = input("Introduce number of columns of matrix 2: ")
 
-x=[]
-for i in range(tam11) :
-	line = []
-	for j in range(tam12) :
-		line.append(input("Introduce position " + str(i) + ", " + str(j)+ ": "))
-	x.append(line)
+		
+		for i in range(tam11) :
+			line = []
+			for j in range(tam12) :
+				line.append(input("Introduce position " + str(i) + ", " + str(j)+ ": "))
+			x.append(line)
 
-y=[]
-for i in range(tam21) :
-	line = []
-	for j in range(tam22) :
-		line.append(input("Introduce position " + str(i) + ", " + str(j) + ": "))
-	y.append(line)
+		
+		for i in range(tam21) :
+			line = []
+			for j in range(tam22) :
+				line.append(input("Introduce position " + str(i) + ", " + str(j) + ": "))
+			y.append(line)
+	elif (salute==2) :
+		nomFich1 = input("Introduce the name of the first file: ")
+		nomFich2 = input("Introduce the name of the second file: ")
+	#Do the multiplication
 
-print x
-print y
-
-if(len(x[0])==len(y)) :
-	print multipliMatrix(x,y)
-else :
-	print "Incompatibles matrix"
+	#print x
+	#print y
+	if(salute!=3) :
+		if(len(x[0])==len(y)) :
+			print multipliMatrix(x,y)
+		else :
+			print "Incompatibles matrix"
+	else :
+		print "Well... See you!"
