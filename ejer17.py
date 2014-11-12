ff2ccb83e7c3cec1a1e7f5e7046d5c5775f5eec1
Pablo Vicente Munuera <pablovm1990@gmail.com>
2014-11-12 20:44:35 +0100

    bug exercise 17

--- ejer17.py
+++ ejer17.py
@@ -3,7 +3,8 @@ import re
 while True : 
 	number = raw_input("Introduce your number: ")
 
-	if re.search('^[0-9]?(([\.][0-9]*([eE][+-]?[0-9]+)?)|([eE][+-]?[0-9]+))$', number) :
+	#
+	if re.search('^[0-9]*(([\.][0-9]*([eE][+-]?[0-9]+)?)|([eE][+-]?[0-9]+))$', number) :
 		print "It is!!!"
 	else :
 		print "It is not!"
