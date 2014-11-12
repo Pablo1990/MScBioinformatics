d211516fdf286091c91c59fbf932494901dcd608
Pablo Vicente Munuera <pablovm1990@gmail.com>
2014-11-12 20:42:38 +0100

    exercise 17 done

---
+++ ejer17.py
@@ -0,0 +1,9 @@
+import re
+
+while True : 
+	number = raw_input("Introduce your number: ")
+
+	if re.search('^[0-9]?(([\.][0-9]*([eE][+-]?[0-9]+)?)|([eE][+-]?[0-9]+))$', number) :
+		print "It is!!!"
+	else :
+		print "It is not!"
