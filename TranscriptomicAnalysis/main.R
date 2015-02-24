576930fb1472500eed9e2bd587783deb562592ce
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-02-24 12:31:57 +0100

    fucking justRMA >.<

--- TranscriptomicAnalysis/main.R
+++ TranscriptomicAnalysis/main.R
@@ -1,3 +1,16 @@
 source("http://bioconductor.org/biocLite.R")
-biocLite("affay")
+library("affy")
+
+#You can find this data in http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE18198
+"multiline
+comment
+"
+dataFiles <- list.files(path="./data", full.names=TRUE, recursive=TRUE)
+data <- c()
+dataFiles[1]
+file <- as.character(dataFiles[1])
+justRMA(filename=dataFiles[1])
+for(dataFile in dataFiles) {
+  data <- c(data,justRMA(dataFile))
+}
 
