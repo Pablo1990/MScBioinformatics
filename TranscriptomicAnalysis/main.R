d7d2b2d39b81195d4587e6b6fbf98bc2dc6fafdd
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-02-25 11:35:44 +0100

    function normalizeData

--- TranscriptomicAnalysis/main.R
+++ TranscriptomicAnalysis/main.R
@@ -1,29 +1,53 @@
-source("http://bioconductor.org/biocLite.R")
-library("affy")
+#source("http://bioconductor.org/biocLite.R")
 
-#You can find this data in http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE18198
-"multiline
-comment
-"
-#Do it with just one directory from one gse, with all, will blow up
-dataFiles <- list.celfiles(path="./data/RawData/GSE18198_RAW", full.names=TRUE, recursive=TRUE)
 
-#From Rma
-"data <- c()
-for(dataFile in dataFiles) {
-  data <- c(data,just.rma(dataFile))
-}"
+normalizeData <- function(directoryName) {
+  library("affy")
+  library("limma")
+  library("genefilter")
+  #You can find this data in http://www.ncbi.nlm.nih.gov/geo/query/acc.cgi?acc=GSE18198
 
-#From Affy
-data <- ReadAffy(filenames=dataFiles)
-
-eset <- expresso(data,
-                 bg.correct = TRUE, 
-                 bgcorrect.method="rma",
-                 normalize = TRUE, 
-                 normalize.method="quantiles", 
-                 pmcorrect.method="pmonly", 
-                 summary.method="medianpolish",
-                 verbose = TRUE,
-) 
-
+  #Do it with just one directory from one gse, with all, will blow up
+  dataFiles <- list.celfiles(path=paste("./data/RawData/",directoryName, sep = ""), full.names=TRUE, recursive=TRUE)
+  
+  #From Rma
+  "data <- c()
+  for(dataFile in dataFiles) {
+    data <- c(data,just.rma(dataFile))
+  }"
+  
+  #From Affy
+  data <- ReadAffy(filenames=dataFiles)
+  
+  #4. Normalizar con RMA 
+  #se crea el objeto eset de clase ExprSet, 
+  #la funcion expresso devuelve los datos de intensidad con transformacion log
+  eset <- expresso(data,
+                   bg.correct = TRUE, 
+                   bgcorrect.method="rma",
+                   normalize = TRUE, 
+                   normalize.method="quantiles", 
+                   pmcorrect.method="pmonly", 
+                   summary.method="medianpolish",
+                   verbose = TRUE,
+  ) 
+  
+  
+  #5. Generar BOXPLOTS antes y despues de normalizar
+  
+  #boxplot de datos brutos
+  boxplot(data,
+          main="Boxplot Before Normalization",
+          col = "lightgrey")
+  
+  
+  #boxplot de datos normalizados		
+  exprseset <- as.data.frame(exprs(eset))		
+  boxplot(data.frame(exprseset),
+          main="Boxplot After Normalization (log scale)",
+          col = "white")
+  
+  
+  #6.Filtrar los datos por IQR.
+  esetIQR <- varFilter(eset, var.func=IQR, var.cutoff=0.5, filterByQuantile=TRUE) 
+}
