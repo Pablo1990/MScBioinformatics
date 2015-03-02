67642e04ac6d98f2b202a483605e81da922b3cff
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-03-02 11:25:09 +0100

    differential expression done

--- TranscriptomicAnalysis/main.R
+++ TranscriptomicAnalysis/main.R
@@ -14,17 +14,19 @@ fileDirs <- list.dirs(path = './data/RawData')
 
 targets18198 <- readTargets("./data/RawData/GSE18198_RAW/targets.txt", row.names="FileName") 
 targets18351 <- readTargets("./data/RawData/GSE18351_RAW/targets.txt", row.names="FileName")
-data <- c()
-for (dir in fileDirs[-1]) {
-  data <- c(data, normalizeData(dir))
-}
 
 #change this to see what we are looking for
-design18198<-cbind(Control=c(1,1,1,1,1,1,0,0,0,0,0,0), Cases=c(0,0,0,0,0,0,1,1,1,1,1,1))
+design <-list(cbind(Control=c(1,1,1,1,1,1,0,0,0,0,0,0), Cases=c(0,0,0,0,0,0,1,1,1,1,1,1)))
 #design18198<-cbind(Control=c(1,1,1,0,0,0,1,1,1,0,0,0), Cases=c(0,0,0,1,1,1,0,0,0,1,1,1))
-differentialExpression(targets18198, data[1], design18198)
 
+design<-list(design, cbind(Control=c(1,1,1,0,0,0), Cases=c(0,0,0,1,1,1)))
 
-design18352<-cbind(Control=c(1,1,1,0,0,0), Cases=c(0,0,0,1,1,1))
-differentialExpression(targets18351, data[2])
-
+data <- c()
+dataDiff <- c()
+cont <- 1
+for (dir in fileDirs[-1]) {
+  aux <- normalizeData(dir)
+  data <- c(data, aux)
+  dataDiff <- c(dataDiff, aux, design[cont])
+  cont <- 2
+}
