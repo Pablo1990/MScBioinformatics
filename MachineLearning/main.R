fb35e1319182071a94d344609474ce603dcce734
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-04-09 12:11:17 +0200

    new dataset

--- MachineLearning/main.R
+++ MachineLearning/main.R
@@ -1,1 +1,9 @@
-library(XML)
+library("AgiMicroRna")
+library("genefilter")
+library("ArrayTools")
+
+targets <- readTargets("datasets/GSE43696_RAW/targets.txt")
+
+dataFiles <- list.files("datasets/GSE43696_RAW/", pattern = "[0-9]+")
+
+data <- readMicroRnaAFE(targets,verbose = T)
