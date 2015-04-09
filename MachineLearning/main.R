21b90b06278d90f7313e29479a591a0842edf16b
Pablo Vicente Munuera <pablovm1990@gmail.com>
2015-04-09 13:31:31 +0200

    one-color agilent read

--- MachineLearning/main.R
+++ MachineLearning/main.R
@@ -1,9 +1,38 @@
-library("AgiMicroRna")
+library("limma")
 library("genefilter")
 library("ArrayTools")
 
-targets <- readTargets("datasets/GSE43696_RAW/targets.txt")
+targets <- readTargets("datasets/GSE43696_RAW/targets.txt", row.names="FileName")
 
 dataFiles <- list.files("datasets/GSE43696_RAW/", pattern = "[0-9]+")
 
-data <- readMicroRnaAFE(targets,verbose = T)
+data <- read.maimages(targets, source="agilent",verbose = T, green.only = T)
+
+eset <- expresso(data,
+                 bg.correct = TRUE, 
+                 bgcorrect.method="rma",
+                 normalize = TRUE, 
+                 normalize.method="quantiles", 
+                 pmcorrect.method="pmonly", 
+                 summary.method="medianpolish",
+                 verbose = TRUE,
+) 
+
+
+#5. Generar BOXPLOTS antes y despues de normalizar
+
+#boxplot de datos brutos
+boxplot(data,
+        main="Boxplot Before Normalization",
+        col = "lightgrey")
+
+
+#boxplot de datos normalizados    
+exprseset <- as.data.frame(exprs(eset))		
+boxplot(data.frame(exprseset),
+        main="Boxplot After Normalization (log scale)",
+        col = "white")
+
+
+#6.Filtrar los datos por IQR.
+esetIQR <- varFilter(eset, var.func=IQR, var.cutoff=0.5, filterByQuantile=TRUE) 
