f338fdd2bf4417d6f5c3db27369d3192399a1ab1
Pablovm1990 <pablovm1990@gmail.com>
2015-04-09 20:16:40 +0200

    first part of the arff

--- MachineLearning/main.R
+++ MachineLearning/main.R
@@ -9,12 +9,34 @@ dataFiles <- list.files("datasets/GSE43696_RAW/", pattern = "[0-9]+")
 
 setwd("~/MScBioinformatics/MachineLearning/datasets/GSE43696_RAW")
 
-data <- read.maimagess(targets, source="agilent",verbose = T, green.only = T)
+data <- read.maimages(targets, source="agilent",verbose = T, green.only = T)
 
 setwd("~/MScBioinformatics/MachineLearning/")
 
-dataNormalized <- backgroundCorrect(data,method="normexp")
+dataNormalized <- backgroundCorrect(data, method="normexp")
+
+#ENorm <- normalizeBetweenArrays(data, method="quantile")
+
+write.csv(dataNormalized$E, "aux.csv")
+
+attributes <- colnames(dataNormalized$E)
+
+attributesStr <- "@RELATION asthma\n"
+
+for (att in attributes){
+  attributesStr <- paste(c(attributesStr,paste(c('@ATTRIBUTE', att, 'numeric'), collapse = ' ')), collapse='\n')
+}
+
+fileConn<-file("final.arff")
+writeLines(attributesStr, fileConn)
+close(fileConn)
+
+for (dato in dataNormalized$E){
+  
+}
 
 
 
-write.arff(dataNormalized,file = "culo.arff")
+#tableData <- read.csv("aux.csv")
+
+#write.arff(dataNormalized$E ,file = "aux.arff")
