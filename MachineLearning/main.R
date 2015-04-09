0c9bff51fedc75af47acbf7bf11e5eebf7106d65
Pablovm1990 <pablovm1990@gmail.com>
2015-04-09 18:10:21 +0200

    normalized data

--- MachineLearning/main.R
+++ MachineLearning/main.R
@@ -1,38 +1,20 @@
 library("limma")
 library("genefilter")
 library("ArrayTools")
+library("RWeka")
 
 targets <- readTargets("datasets/GSE43696_RAW/targets.txt", row.names="FileName")
 
 dataFiles <- list.files("datasets/GSE43696_RAW/", pattern = "[0-9]+")
 
-data <- read.maimages(targets, source="agilent",verbose = T, green.only = T)
+setwd("~/MScBioinformatics/MachineLearning/datasets/GSE43696_RAW")
 
-eset <- expresso(data,
-                 bg.correct = TRUE, 
-                 bgcorrect.method="rma",
-                 normalize = TRUE, 
-                 normalize.method="quantiles", 
-                 pmcorrect.method="pmonly", 
-                 summary.method="medianpolish",
-                 verbose = TRUE,
-) 
+data <- read.maimagess(targets, source="agilent",verbose = T, green.only = T)
+
+setwd("~/MScBioinformatics/MachineLearning/")
+
+dataNormalized <- backgroundCorrect(data,method="normexp")
 
 
-#5. Generar BOXPLOTS antes y despues de normalizar
 
-#boxplot de datos brutos
-boxplot(data,
-        main="Boxplot Before Normalization",
-        col = "lightgrey")
-
-
-#boxplot de datos normalizados    
-exprseset <- as.data.frame(exprs(eset))		
-boxplot(data.frame(exprseset),
-        main="Boxplot After Normalization (log scale)",
-        col = "white")
-
-
-#6.Filtrar los datos por IQR.
-esetIQR <- varFilter(eset, var.func=IQR, var.cutoff=0.5, filterByQuantile=TRUE) 
+write.arff(dataNormalized,file = "culo.arff")
